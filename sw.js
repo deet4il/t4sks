3bba2099c9b5d5e34af599e4d3a0e3185d4f9b34
deet4il <novembergray1992@gmail.com>
2026-05-01 22:25:28 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-// The "Engine" that makes your app work offline
+const CACHE_NAME = 'gwapo-v1';
+
+// We "install" the service worker and create a cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(['./']);
+    })
+  );
+});
+
+// We catch the fetch request to satisfy Chrome's "Offline" requirement
 self.addEventListener('fetch', (event) => {
-  // We're leaving this empty to ensure your tasks update instantly
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
